6085e0ac6c0f57bd3cdc6d6203c4a46508dc280c
Sungho, Kim <57740411+opp-13@users.noreply.github.com>
2024-10-16 13:17:49 +0900

    Create minikube_install.sh
    Add minikube bash

---
+++ minikube_install.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+if [ -f /etc/debian_version ]; then
+    echo "ubuntu found"
+    
+    if [ "$(id -u)" -ne 0 ]; then
+        echo "Please run this bash in root."
+        exit 1
+    fi
+
+    sudo apt-get update
+    sudo apt-get upgrade -y
+
+    sudo apt-get install curl
+
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+    elif [ -f /etc/redhat-release ]; then
+        echo "rehl found"
+
+    if [ "$(id -u)" -ne 0 ]; then
+        echo "Please run this bash in root."
+        exit 1
+    fi
+
+    sudo yum update -y
+
+    sudo yum install curl 
+
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+    sudo yum install -y yum-utils
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+    sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+else
+    echo "Only support Debian/Rehl"
+fi
+
+sudo systemctl start docker
